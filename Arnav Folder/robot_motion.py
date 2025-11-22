722b58e33bde2a294c7a1f809d9b617083afa377
arnav <21arnav10@gmail.com>
2025-11-22 17:00:35 -0500

    Added pen with URDF and some test objects.
    Wasn't able to test the pen physics since I am having some issue with spawning in items and trying to debug

--- Arnav Folder/robot_motion.py
+++ Arnav Folder/robot_motion.py
@@ -9,6 +9,9 @@ ground = m.Ground()
 # Create table
 table = m.URDF(filename=os.path.join(m.directory, 'table', 'table.urdf'), static=True, position=[0, 0, 0], orientation=[0, 0, 0, 1])
 
+# Create pen
+bowl = m.URDF(filename='bowl.urdf', static=False, position=[0,0,1], orientation=[0, 0, 0, 1])
+
 # Create Panda manipulator
 robot = m.Robot.Panda(position=[0.5, 0, 0.75])
 
@@ -17,18 +20,3 @@ pos = [0, 0, 0.8]
 orient = m.get_quaternion([np.pi, 0, 0])
 target_joint_angles = robot.ik(robot.end_effector, target_pos=pos, target_orient=orient)
 robot.control(target_joint_angles, set_instantly=True)
-
-for i in range(25):
-    # Move the end effector to a new pose
-    target_joint_angles = robot.ik(robot.end_effector, target_pos=[0.2, 0.2, 0.8], target_orient=m.get_quaternion([np.pi, np.pi/4, 0]), use_current_joint_angles=True)
-    robot.control(target_joint_angles)
-
-    v = robot.get_joint_velocities(include_fixed_joints=False)
-    J = robot.get_linear_jacobian(robot.end_effector)
-    v_G = robot.get_link_velocity(robot.end_effector)
-
-    # Is v_G = J.dot(v) ?
-    print('Equal:', np.allclose(v_G, J.dot(v)), '\n\t v_G', v_G, '\n\t J.v', J.dot(v))
-
-    m.step_simulation(realtime=True)
-
